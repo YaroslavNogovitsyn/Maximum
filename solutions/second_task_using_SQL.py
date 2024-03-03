779084e28cdc83f292e1c0ddff7eb2281ba565f0
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2024-03-03 09:37:03 +0300

    fix: new sql query

--- solutions/second_task_using_SQL.py
+++ solutions/second_task_using_SQL.py
@@ -3,26 +3,26 @@ import pandas as pd
 from config import sync_engine
 
 sql_query = """
-    WITH cte AS (SELECT MAX(s.visitor_session_id)   AS visitor_session_id,
-                    c.visitor_id,
-                    s.site_id,
+WITH cte AS (SELECT MAX(c.communication_id)     as communication_id,
+                    MAX(s.visitor_session_id)   AS visitor_session_id,
                     COUNT(DISTINCT s.date_time) AS row_n,
                     MAX(s.date_time)            AS session_date_time
              FROM web_data.communications c
-                      LEFT JOIN web_data.sessions s ON c.visitor_id = s.visitor_id AND c.site_id = s.site_id AND c.date_time >= s.date_time
-             GROUP BY c.visitor_id, s.site_id)
-SELECT c.communication_id,
-       c.site_id,
-       c.visitor_id,
-       c.date_time AS communication_date_time,
-       s.visitor_session_id,
-       cte.session_date_time,
-       s.campaign_id,
-       cte.row_n
-FROM web_data.sessions s
-         INNER JOIN web_data.communications c
-                    ON s.visitor_id = c.visitor_id AND s.site_id = c.site_id
-         INNER JOIN cte ON cte.visitor_session_id = s.visitor_session_id;
+                      LEFT JOIN web_data.sessions s
+                                 ON s.visitor_id = c.visitor_id AND s.site_id = c.site_id AND c.date_time > s.date_time
+             GROUP BY c.communication_id)
+SELECT DISTINCT cte.communication_id,
+                c.site_id,
+                c.visitor_id,
+                c.date_time AS communication_date_time,
+                cte.visitor_session_id,
+                cte.session_date_time,
+                s.campaign_id,
+                cte.row_n
+FROM web_data.communications c
+         INNER JOIN cte ON cte.communication_id = c.communication_id
+         LEFT JOIN web_data.sessions s
+                   ON s.visitor_id = c.visitor_id AND s.site_id = c.site_id AND cte.session_date_time = s.date_time;
 """
 
 with sync_engine.connect() as conn:
